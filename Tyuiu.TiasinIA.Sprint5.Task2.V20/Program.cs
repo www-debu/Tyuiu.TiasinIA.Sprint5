b7bfda2ae25aa003397c1b400d3e99987a707fcc
Илья Тясин <turbopad7200@gmail.com>
2024-10-15 17:34:03 +0500

    Task2

--- Tyuiu.TiasinIA.Sprint5.Task2.V20/Program.cs
+++ Tyuiu.TiasinIA.Sprint5.Task2.V20/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Tyuiu.TiasinIA.Sprint5.Task2.V20
+﻿using Tyuiu.TiasinIA.Sprint5.Task2.V20.Lib;
+namespace Tyuiu.TiasinIA.Sprint5.Task2.V20
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнила: Тясин И. А. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл      *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнила: Тясин Илья Александрович | АСОиУб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов                     *");
+            Console.WriteLine("* Заменить положительные элементы массива на 1, отрицательные на 0.       *");
+            Console.WriteLine("* Результат сохранить в файл OutPutFileTask2.csv и вывести на консоль.    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] matrix = new int[3, 3] { { 5, -5, -4 }, { -4, 2, -4 }, { -7, 1, 4 } };
+
+            int rows = matrix.GetUpperBound(0) + 1;
+            int cols = matrix.Length / rows;
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{matrix[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(matrix);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
         }
     }
 }
