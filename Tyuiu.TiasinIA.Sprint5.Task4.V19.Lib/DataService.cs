c12134a9c6dfdf08a72e92920fab12d643dda13b
Илья Тясин <turbopad7200@gmail.com>
2024-10-21 21:01:28 +0500

    Task4

--- Tyuiu.TiasinIA.Sprint5.Task4.V19.Lib/DataService.cs
+++ Tyuiu.TiasinIA.Sprint5.Task4.V19.Lib/DataService.cs
@@ -22,9 +22,10 @@ namespace Tyuiu.TiasinIA.Sprint5.Task4.V19.Lib
                 content = content.Replace(',', '.');
             }
 
-            double y = Math.Sin(Math.Pow(x, 3)) + (2 / x);
+            double y = Math.Round(Math.Pow(Convert.ToDouble(content) / Math.Cos(Convert.ToDouble(content)), 2), 3);
 
             return Math.Round(y, 3);
         }
     }
 }
+
